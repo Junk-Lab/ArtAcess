07c443eb8eb3ae3c7fc3a9304835f858aed784fe
JunkLab <richard.ms.torres@gmail.com>
2023-07-11 04:07:54 -0300

    andamento da pagina home, add funçoes nos botoes do header

--- public/Script/login.js
+++ public/Script/login.js
@@ -1,4 +1,6 @@
 
+//-------------------------- Header-------------------------- 
+
   //função para redirecionar da tela de login para a tela homepage clicando no incone 
     function redihome() {
     var pagehome = "http://localhost:3000/home";
@@ -18,6 +20,7 @@
   }
 
   //função para fazer o redirecionamento da pagina de login para a pagina de 'soluções'
+    
     function solutions() {
       var Solutions = "http://localhost:3000/register";
       window.location.href = Solutions;
@@ -51,35 +54,104 @@
 
       var buttonregister = document.getElementById("Register");
       buttonregister.addEventListener("click", register);
-
   }
+
+  //-------------------------- Login -------------------------- 
 
   //função que faz a verificação de login ( ainda em desenvolvimento )
     function login() {
-    var username = document.getElementById("UserLogin").value;
-    var password = document.getElementById("PasswordLogin").value;
+      var username = document.getElementById("UserLogin").value;
+      var password = document.getElementById("PasswordLogin").value;
     
-  function showAlert(LoginErro){
-    var alertDiv = document.getElementById('AlertLog')
-    alertDiv.innerText = LoginErro;
-    }
+    function showAlert(LoginErro){
+      var alertDiv = document.getElementById('AlertLog')
+      alertDiv.innerText = LoginErro;
+      }
 
-    if (username === "junklab" && password === "0000") {
-      window.location.href = "/profile"
+      if (username === "junklab" && password === "0000") {
+        window.location.href = "/profile"
 
-    } else {
-      showAlert("Credenciais inválidas.")     
-    }
+      } else {
+        showAlert("Credenciais inválidas.")     
+      }
   }
+
+    function forgotpass () {
+      var ForgotPass = "http://localhost:3000/register";
+      window.location.href = ForgotPass;
   
-  //função que faz ir da pagina login para a pagina do 'YouTube'
-    function SM0() {
+      var buttonforgotpass = document.getElementById("ForgotPass");
+      buttonforgotpass.addEventListener("click", forgotpass);
+  }
+
+  //-------------------------- Footer Link Politicos Advertising -------------------------- 
+
+function advertising () {
+  var Advertising = "http://localhost:3000/register";
+  window.location.href = Advertising;
+
+  var buttonAdvertising = document.getElementById("Advertising");
+  buttonAdvertising.addEventListener("click", advertising);
+}
+
+function cookeis () {
+  var Cookeis = "http://localhost:3000/register";
+  window.location.href = Cookeis;
+
+  var buttoncookies = document.getElementById("Cookies");
+  buttoncookies.addEventListener("click", cookeis);
+}
+ 
+function privacy () {
+  var Privacy = "http://localhost:3000/register";
+  window.location.href = Privacy;
+
+  var buttonprivacy = document.getElementById("Privacy");
+  buttonprivacy.addEventListener("click", privacy);
+}
+ 
+function terms () {
+  var Terms = "http://localhost:3000/register";
+  window.location.href = Terms;
+
+  var buttonterms = document.getElementById("Terms");
+  buttonterms.addEventListener("click", terms);
+}
+
+//-------------------------- Footer Social Midia -------------------------- 
+
+  //função que faz ir da pagina 'login' para a pagina do 'YouTube'
+    function sm0 () {
       var youtube = "http://localhost:3000/register";
       window.location.href = youtube;
 
       var buttonSM0 = document.getElementById("SM0");
-      buttonSM0.addEventListener("click", SM0)
+      buttonSM0.addEventListener("click", sm0);
+  }
 
-    }
+    //função que faz ir da pagina login para a pagina do 'YouTube'
+    function sm1 () {
+      var facebook  = "http://localhost:3000/register";
+      window.location.href = facebook;
+
+      var buttonSM0 = document.getElementById("SM1");
+      buttonSM0.addEventListener("click", sm1);
+  }
   
+    //função que faz ir da pagina login para a pagina do 'YouTube'
+    function sm2 () {
+      var linkedin = "http://localhost:3000/register";
+      window.location.href = linkedin;
+  
+      var buttonSM0 = document.getElementById("SM2");
+      buttonSM0.addEventListener("click", sm2);
+  }
 
+    //função que faz ir da pagina login para a pagina do 'YouTube'
+    function sm3 () {
+      var reddit = "http://localhost:3000/register";
+      window.location.href = reddit;
+
+      var buttonSM0 = document.getElementById("SM3");
+      buttonSM0.addEventListener("click", sm3);
+  }
